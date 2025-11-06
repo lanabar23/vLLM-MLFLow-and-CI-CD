4fc75bb53afefd587da20cd9eae8c8b84fa1ad8d
lanabar23 <lanabar69@gmail.com>
2025-11-06 18:57:49 +0300

    Добавлен ping IP виртуальной машины

---
+++ tests/test_ping.py
@@ -0,0 +1,15 @@
+import pytest
+import subprocess
+
+@pytest.fixture
+def server_url():
+    return "176.108.250.95"  # Замените на ваш IP-адрес
+
+def test_ping_server(server_url):
+    try:
+        # Выполняем команду ping
+        result = subprocess.run(['ping', '-c', '4', server_url], capture_output=True, text=True, check=True)
+        # Проверяем, что ping прошел успешно
+        assert "4 packets transmitted, 4 received" in result.stdout
+    except subprocess.CalledProcessError as e:
+        pytest.fail(f"Ошибка при выполнении ping: {e}")
