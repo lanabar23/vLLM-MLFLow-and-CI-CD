0ceaf766789979c51b4256a4004e9fc864a36480
lanabar23 <lanabar69@gmail.com>
2025-11-07 05:33:17 +0300

    Изменено имя с server_url на url

--- tests/test_client_server.py
+++ tests/test_client_server.py
@@ -13,11 +13,11 @@ print(your_server_url)
 # def server_url():
 #     return f'http://{SERVER_URL}'  #{your_server_url}'
 
-def test_get_data(server_url):
-    logging.debug(f"Отправляем GET-запрос на {server_url}/data")
+def test_get_data(url):
+    logging.debug(f"Отправляем GET-запрос на {url}/data")
     print('get')
     try:
-        response = requests.get(f"{server_url}/data", timeout=10)
+        response = requests.get(f"{url}/data", timeout=10)
         logging.debug(f"Получен ответ с кодом {response.status_code}")
         assert response.status_code == 200
         assert isinstance(response.json(), dict)
@@ -25,12 +25,12 @@ def test_get_data(server_url):
         logging.error(f"Ошибка при отправке GET-запроса: {e}")
         #pytest.fail(f"Ошибка при отправке GET-запроса: {e}")
 
-def test_post_data(server_url):
+def test_post_data(url):
     payload = {'field': 'value'}
-    logging.debug(f"Отправляем POST-запрос на {server_url}/post-data с данными: {payload}")
+    logging.debug(f"Отправляем POST-запрос на {url}/post-data с данными: {payload}")
     print('post')
     try:
-        response = requests.post(f"{server_url}/post-data", json=payload, timeout=10)
+        response = requests.post(f"{url}/post-data", json=payload, timeout=10)
         logging.debug(f"Получен ответ с кодом {response.status_code}")
         assert response.status_code == 201
         assert response.json()['message'] == 'Success!'
